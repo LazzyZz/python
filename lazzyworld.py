fae00d78553e5b172c7f209603782e320c958a4f
LazzyJob's <105605853+LazzyZz@users.noreply.github.com>
2022-05-24 17:06:40 +0400

    Update lazzyworld.py

--- lazzyworld.py
+++ lazzyworld.py
@@ -1,5 +1,5 @@
 def helloworld(str: str = 'Hellow, world !', i : int = 0, a : int = 10):
-	q = [print(str, "create LazzyZzJob's") for q in range(i, a)]
+	q = [print(str, "create LazzyJob's") for q in range(i, a)]
 	return str, i, a, q
 
 helloworld()
