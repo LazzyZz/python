45b9eec31ec3c6a6748b3a85359233b52e2cc61c
LazzyJob's <105605853+LazzyZz@users.noreply.github.com>
2022-06-20 11:12:49 +0400

    Update say helloworld.py

--- say helloworld.py
+++ say helloworld.py
@@ -1,7 +1,7 @@
 text = input("Say anything ?: ")
 #1
 def helloworld(str1: str = 'Hello, world !', i : int = 0, a : int = 10):
-	return [print(str1, "created LazzyZzJob's") for q in range(i, a)]
+	return (print(str1, "created LazzyZzJob's") for q in range(i, a))
 helloworld(text)
 #2
 x1 = lambda str1 = "Hello, world !", i = int(1), a = int(10) : (print(str1, "created LazzyZzJob's") for q in range(i, a))
